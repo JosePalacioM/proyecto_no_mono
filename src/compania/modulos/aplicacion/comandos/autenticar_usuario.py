d6175026cad9c7c4217d2233456ec4eb67b2bf2a
jpalacio-bv <jose.palacio@bazaarvoice.com>
2024-03-02 22:51:46 -0500

    Revert "Se agrega codigo de compañía"
    This reverts commit ff8e88f883d850a9d26a4f445dac87bbac446fbb.

--- src/compania/modulos/aplicacion/comandos/autenticar_usuario.py
+++
@@ -1,9 +0,0 @@
-from compania.seedwork.aplicacion.comandos import Comando, ComandoHandler
-
-@dataclass
-class ComandoAutenticarUsuario(Comando):
-    email: str
-    password: str
-
-class AutenticarUsuarioHandler(ComandoHandler):
-    ...
